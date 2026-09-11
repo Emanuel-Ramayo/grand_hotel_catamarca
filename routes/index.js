56265655eea36d0494ceadfe8c62ccc182f17d42
Emanuel Ramayo <emanuelramayo12@gmail.com>
2026-09-11 09:32:57 -0300

    update: modififcar ruta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var resenasData = require('../data/resenas.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { 
+    resenas: resenasData 
+  });
 });
 
 module.exports = router;
